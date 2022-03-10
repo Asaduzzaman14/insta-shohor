68bb6c3dae4599171ddb464a55fc506b311f3b44
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-10 23:07:39 +0600

    report-section-modefi

--- js/app.js
+++ js/app.js
@@ -150,6 +150,8 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML = ''
+
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -159,6 +161,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
+  console.log(reportedPosts);
   posts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
